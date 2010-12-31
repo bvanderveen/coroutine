74158d55d6d446997ecb77629fe2ae4c4e183771
Benjamin van der Veen <bvanderveen@bvanderveen.local>
2010-12-30 19:33:10 -0800

    remove old coroutine

--- src/Coroutine/Coroutine.cs
+++ src/Coroutine/Coroutine.cs
@@ -7,14 +7,6 @@ namespace Coroutine
 {
     public delegate void Continuation(Action<object> complete, Action<Exception> exception);
 
-
-    //public interface IContinuationState<out T>
-    //{
-    //    Continuation Continuation { get; }
-    //    T Result { get; }
-    //}
-
-
     public class ContinuationState
     {
         internal Continuation Continuation;
@@ -23,13 +15,14 @@ namespace Coroutine
             Continuation = c;
         }
     }
+
     public class ContinuationState<T> : ContinuationState
     {
         public ContinuationState(Continuation c) : base(c) { }
-        public T Result { get { if (Coroutine2.exceptionState != null) throw Coroutine2.exceptionState; return (T)Coroutine2.resultState; } }
+        public T Result { get { if (Coroutine.exceptionState != null) throw Coroutine.exceptionState; return (T)Coroutine.resultState; } }
     }
 
-    public static class Coroutine2
+    public static class Coroutine
     {
         public static Task<object> AsTask(this Continuation cont)
         {
@@ -94,7 +87,6 @@ namespace Coroutine
             catch (Exception e)
             {
                 //Console.WriteLine("Exception during MoveNext.");
-                Console.WriteLine("Exit location 1.");
                 continuation.Dispose();
                 exception(e);
                 return;
@@ -103,7 +95,6 @@ namespace Coroutine
             if (!continues)
             {
                 //Console.WriteLine("Continuation does not continue.");
-                Console.WriteLine("Exit location 2.");
                 continuation.Dispose();
                 //result(null);
                 return;
@@ -147,108 +138,8 @@ namespace Coroutine
             }
             catch (Exception e)
             {
-                Console.WriteLine("Exit location 3.");
                 continuation.Dispose();
                 exception(e);
-                return;
-            }
-        }
-    }
-
-    public class Coroutine<T>
-    {
-        IEnumerator<object> continuation;
-        AsyncResult<T> asyncResult;
-        TaskScheduler scheduler;
-
-        public Coroutine(IEnumerator<object> continuation, TaskScheduler scheduler)
-        {
-            this.continuation = continuation;
-            this.scheduler = scheduler;
-        }
-
-        public IAsyncResult BeginInvoke(AsyncCallback cb, object state)
-        {
-            asyncResult = new AsyncResult<T>(cb, state);
-
-            Continue(true);
-
-            return asyncResult;
-        }
-
-        public T EndInvoke(IAsyncResult result)
-        {
-            //Console.WriteLine("Coroutine.EndInvoke");
-            AsyncResult<T> ar = (AsyncResult<T>)result;
-            continuation.Dispose();
-            return ar.EndInvoke();
-        }
-
-        void Continue(bool sync)
-        {
-            //Console.WriteLine("Continuing!");
-            var continues = false;
-
-            try
-            {
-                continues = continuation.MoveNext();
-            }
-            catch (Exception e)
-            {
-                //Console.WriteLine("Exception during MoveNext.");
-                asyncResult.SetAsCompleted(e, sync);
-                return;
-            }
-
-            if (!continues)
-            {
-                //Console.WriteLine("Continuation does not continue.");
-                asyncResult.SetAsCompleted(null, sync);
-                return;
-            }
-
-            try
-            {
-                var value = continuation.Current;
-
-                var task = value as Task;
-
-                if (task != null)
-                {
-                    //Console.WriteLine("Will continue after Task.");
-                    task.ContinueWith(t =>
-                    {
-                        bool synch = ((IAsyncResult)t).CompletedSynchronously;
-
-                        if (false && t.IsFaulted) // disabled, iterator blocks must always remember to check for exceptions
-                        {
-                            //Console.WriteLine("Exception in Task.");
-                            //Console.Out.WriteException(t.Exception);
-                            asyncResult.SetAsCompleted(t.Exception, sync);
-                        }
-                        else
-                        {
-                            //Console.WriteLine("Continuing after Task.");
-                            Continue(sync);
-                        }
-                    }, CancellationToken.None, TaskContinuationOptions.None, scheduler);
-                }
-                else
-                {
-                    if (value is T)
-                    {
-                        //Console.WriteLine("Completing with value.");
-                        asyncResult.SetAsCompleted((T)value, sync);
-                        return;
-                    }
-
-                    //Console.WriteLine("Continuing, discarding value.");
-                    Continue(sync);
-                }
-            }
-            catch (Exception e)
-            {
-                asyncResult.SetAsCompleted(e, sync);
                 return;
             }
         }
--- src/Coroutine/Extensions.cs
+++ src/Coroutine/Extensions.cs
@@ -9,92 +9,6 @@ namespace Coroutine
 {
     public static partial class Extensions
     {
-        public static Task<T> AsCoroutine<T>(this IEnumerable<object> iteratorBlock)
-        {
-            return iteratorBlock.AsCoroutine<T>(TaskScheduler.Current);
-        }
-
-        public static Task<T> AsCoroutine<T>(this IEnumerable<object> iteratorBlock, TaskScheduler scheduler)
-        {
-            return iteratorBlock.AsCoroutine<T>(TaskCreationOptions.None, scheduler);
-        }
-
-        /// <summary>
-        /// Creates a Coroutine Task from the iterator block. 
-        /// </summary>
-        /// <typeparam name="T">The type of the expected return value of the Coroutine.</typeparam>
-        /// <param name="iteratorBlock">The iterator block.</param>
-        /// <param name="opts">The <cref>TaskCreationOptions</cref> for the Coroutine Task.</param>
-        /// <param name="scheduler">The <cref>TaskScheduler</cref> on which the Coroutine should execute.</param>
-        /// <returns></returns>
-        public static Task<T> AsCoroutine<T>(this IEnumerable<object> iteratorBlock, TaskCreationOptions opts, TaskScheduler scheduler)
-        {
-            var coroutine = new Coroutine<T>(iteratorBlock.GetEnumerator(), scheduler);
-
-            var cs = new TaskCompletionSource<T>(opts);
-
-            coroutine.BeginInvoke(iasr =>
-            {
-                try
-                {
-                    cs.SetResult(coroutine.EndInvoke(iasr));
-                }
-                catch (Exception e)
-                {
-                    cs.SetException(e);
-                }
-            }, null);
-
-            return cs.Task;
-        }
-        
-        /// <summary>
-        /// Returns Task&lt;object&gt; if the argument is a Task&lt;T&gt;, otherwise null.
-        /// </summary>
-        /// <param name="task"></param>
-        /// <returns></returns>
-        public static Task<object> AsTaskWithValue(this Task task)
-        {
-            // well, this is significantly less painful than the observable version...
-
-            var taskType = task.GetType();
-
-            if (!taskType.IsGenericType) return null;
-
-            var tcs = new TaskCompletionSource<object>();
-            task.ContinueWith(t =>
-            {
-                if (task.IsFaulted)
-                    tcs.SetException(task.Exception);
-                else
-                    tcs.SetResult(taskType.GetProperty("Result").GetGetMethod().Invoke(task, null));
-            });
-            return tcs.Task;
-        }
-
-        public static Task<T> CreateCoroutine<T>(this IEnumerable<object> iteratorBlock)
-        {
-            return iteratorBlock.CreateCoroutine<T>(TaskScheduler.Current);
-        }
-
-        public static Task<T> CreateCoroutine<T>(this IEnumerable<object> iteratorBlock, TaskScheduler scheduler)
-        {
-            var taskFactory = new TaskFactory(scheduler);
-            var tcs = new TaskCompletionSource<T>();
-
-            taskFactory.StartNew(() => iteratorBlock.AsCoroutine<T>().ContinueWith(t
-                =>
-            {
-                if (t.IsFaulted)
-                    tcs.SetException(t.Exception.InnerExceptions);
-                else
-                    tcs.SetResult(t.Result);
-            }));
-
-            return tcs.Task;
-        }
-
-
         public static Task<object> StartCoroutineTask(this IEnumerable<object> iteratorBlock)
         {
             return iteratorBlock.StartCoroutineTask(TaskScheduler.Current);
@@ -155,13 +69,13 @@ namespace Coroutine
         public static void BeginCoroutine(this IEnumerable<object> iteratorBlock,
             Action<object> result, Action<Exception> exception)
         {
-            Coroutine2.Continue(iteratorBlock.GetEnumerator(), result, exception, null);
+            Coroutine.Continue(iteratorBlock.GetEnumerator(), result, exception, null);
         }
 
         public static void BeginCoroutine(this IEnumerable<object> iteratorBlock,
             Action<object> result, Action<Exception> exception, Action<Action> trampoline)
         {
-            Coroutine2.Continue(iteratorBlock.GetEnumerator(), result, exception, trampoline);
+            Coroutine.Continue(iteratorBlock.GetEnumerator(), result, exception, trampoline);
         }
     }
 }
